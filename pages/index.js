671233ace7cac350b4b3732c354b1d5fefe4fa7d
pedroallima <pedro.leao@mercadolivre.com>
2022-05-24 08:20:50 -0300

    subiu

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'; // get the React object from the react module
+
+class HelloWorld extends React.Component {
+  render() {
+    return <p>Hello, world!</p>;
+  }
+}
+
+export default HelloWorld;
+
